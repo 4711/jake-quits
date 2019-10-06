cffbcb9e03e811859be9810024d1982afe4e987f
4711 <bzthcyqullt@mailinator.com>
2019-10-06 21:54:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const data = {
   dollars: null,
   cents: null,
   dailyCost: 6.375466667,
-  //time: 1569369600000 // new Date(2019, 8, 24, 19)
+  //time: 1569369600000 // new Date(1963, 10, 3, 19)
   time: new Date(2000, 1, 1, 19)
 };
 
